c3f98395f45fa1cde8273331e52cb29e3c9f77a6
bakibayram0 <bakibayram347@gmail.com>
2022-10-21 21:40:31 +0300

    odev3

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,23 +1,88 @@
 var express = require('express');
 var router = express.Router();
 
-const anaSayfa = function (req, res,) {
-  res.render('anasayfa', { 'title': 'Ana sayfa' });
+//anasayfa ismi sabit kalması için const yapılır
+
+const anaSayfa = function (req, res, next) {
+  res.render('anasayfa',
+    {
+      "baslik": 'Anasayfa',
+      "sayfaBaslik": {
+        "siteAd": "MekanBul",
+        "slogan": "Civardaki Mekanları Kesfet"
+      },
+      "mekanlar":[
+        {
+          "ad":"Starbucks",
+          "puan":"5",
+          "adres":"Centrum Garden AVM",
+          "imkanlar":["Kahve","Çay","Kek"],
+          "mesafe":"13km"
+        },
+        {
+          "ad":"Arabica",
+          "puan":"4",
+          "adres":"İyaş Awm",
+          "imkanlar":["Kahve","Çay"],
+          "mesafe":"2km"
+        }
+        
+      ]
+
+    });
 }
 
-const mekanBilgisi = function (req, res,) {
-  res.render('mekanbilgisi', { 'title': 'Mekan bilgisi' });
+const mekanBilgisi = function (req, res, next) {
+  res.render('mekanbilgisi', 
+  { "baslik": 'Mekan Bilgisi',
+  "mekanBaslik":"Starbucks",
+  "mekanDetay":{
+    "ad":"Starbucks",
+    "puan":"5",
+    "adres":"Centrum Garden AVM",
+    "saatler":[
+      {
+        "gunler":"Pazartesi - Cuma",
+        "acilis":"9:00-23:00",
+        "kapali":"false"
+      },
+      {
+        "gunler":"Cumartesi - Pazar",
+        "acilis":"08:00-22:00",
+        "kapali":"false"
+      }
+
+    ],
+    "imkanlar":["Kahve","Çay","Kek"],
+    "koordinatlar":{
+      "enlem":"37.7",
+      "boylam":"30.5"
+    },
+    "yorumlar":[
+      {
+        "yorumYapan":"Abdulbaki Bayram",
+        "yorumMetni":"Kahveleri çok güzeldi",
+        "tarih":"21 Ekim 2022",
+        "puan":"5"
+      },
+      {
+        "yorumYapan":"Dilara Erdoğan",
+        "yorumMetni":"Kahveleri Beğenmedim",
+        "tarih":"21 Ekim 2022",
+        "puan":"1"
+      }
+    ]
+  } 
+});
 }
 
-const yorumEkle = function (req, res,) {
-  res.render('yorumekle', { 'title': 'Yorum ekle' });
+const yorumEkle = function (req, res, next) {
+  res.render('yorumekle', { title: 'Yorum Ekle' });
 }
 
+
+// anasayfayı dış dünyaya açmak için 
 
 module.exports = {
-
-  anaSayfa,
-  mekanBilgisi,
-  yorumEkle,
-
+  anaSayfa, mekanBilgisi, yorumEkle
 }
